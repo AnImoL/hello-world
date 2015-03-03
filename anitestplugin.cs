80dc27ca89ea12fc35121943c2993f4ce9b0d7f9
AnImoL <animol3@hotmail.com>
2015-03-03 13:46:00 -0300

    Create anitestplugin.cs

---
+++ anitestplugin.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI; /* This requires TShockAPI.dll as reference */
+using Terraria; /* This requires TerrariaServer.exe as reference */
+ 
+namespace TestPlugin
+{
+    [ApiVersion(1, 16)] /* The version of TerrariaServer.exe (or the API, currently 1.16.0.0) */
+    public class TestPlugin : TerrariaPlugin
+    {
+        public TestPlugin(Main game) : base(game)
+        {
+        }
+        public override void Initialize()
+        {
+        }
+        public override Version Version
+        {
+            get { return new Version("1.0"); } /* YOu can change this as your version improves */
+        }
+        public override string Name
+        {
+            get { return "TestPlugin"; } /* The name of your plugin */
+        }
+        public override string Author
+        {
+            get { return "AnImoL"; } /* Your name */
+        }
+        public override string Description
+        {
+            get { return "Testing!"; } /* Description... gotta get more info */
+        }
+    }
+}
